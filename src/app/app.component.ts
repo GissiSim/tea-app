398c56fbcd8956f11def99d58c8cfb47d6e9f198
Gissur Simonarson <gissisim@gmail.com>
2017-08-08 10:24:23 +0100

    observable example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core'
 
+import * as Rx from 'rxjs'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  event
+
+  constructor() {
+    const $body = document.body
+    const watchMouse = Rx.Observable.fromEvent($body, 'mousemove')
+    watchMouse.subscribe(event => (this.event = event))
+  }
+}
